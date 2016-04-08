ece4428e8571568e88eb2d31f69c998c2c5fccdb
Daphne Ippolito <daphnei@seas.upenn.edu>
2016-04-08 10:38:08 -0400

    learned how to do app enginer server stuff

--- Application/src/main/java/com/example/android/camera2video/EndpointAsyncTask.java
+++ Application/src/main/java/com/example/android/camera2video/EndpointAsyncTask.java
@@ -21,18 +21,11 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
     @Override
     protected String doInBackground(Pair<Context, String>... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            MyApi.Builder builder =
+                    new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null);
+
+            builder.setRootUrl("https://canaryproject-1275.appspot.com/_ah/api/");
+
             // end options for devappserver
             builder.setApplicationName("Bird app");
 
